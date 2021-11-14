3fdbfe9892eeddcedc1e01ceb92681ffd61bed1d
aviadgawl <aviadgawl@gmail.com>
2021-11-14 17:24:14 +0200

    fixing the AddChampion component

--- src/components/champion-add/ChampionAdd.js
+++ src/components/champion-add/ChampionAdd.js
@@ -1,12 +1,17 @@
 import './ChampionAdd.css';
 
+import { useRef } from 'react';
 import StatButton from '../stat-button/StatButton';
 
 export default function ChampionAdd(props) {
-    const championNamesList = ['David' , 'Shimshon' , 'The Rock' , 'Mor' , 'Sigalit' , 'Aviad'];
+    const championNamesList = useRef(['David', 'Shimshon', 'The Rock', 'Mor', 'Sigalit', 'Aviad']);
 
     const handelOnPress = () => {
-        props.onPress(championNamesList[0]);
+        if (championNamesList.current.length === 0) return;
+
+        const randomNumber = Math.floor(Math.random() * championNamesList.current.length);
+        const selectedChampionName = championNamesList.current.splice(randomNumber, 1)
+        props.onPress(selectedChampionName);
     }
 
     return (<div className="champion-add">
